1b6862d74e50671419d82e6f34f82abdfa4f27c7
Sebastianpc2001 <73415184+Sebastianpc2001@users.noreply.github.com>
2025-04-09 18:34:31 +0200

    Correction in spelling in final message

--- Calculadora/multiplication.py
+++ Calculadora/multiplication.py
@@ -1,4 +1,4 @@
 value_1 = int(input("The first value to multiply: "))
 value_2 = int(input("The second value to multiply: "))
 
-print(f"The sum of {value_1} + {value_2} is: {value_1*value_2}")
+print(f"The multiplication of {value_1} times {value_2} is: {value_1*value_2}")
