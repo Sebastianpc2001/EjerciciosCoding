d224549701be72ef71f7c734bed39707d4d07707
Sebastianpc2001 <73415184+Sebastianpc2001@users.noreply.github.com>
2025-04-22 11:51:28 +0200

    Progress in Pokemon API

--- first-app/app/home/page.tsx
+++ first-app/app/home/page.tsx
@@ -1,16 +1,32 @@
-import React from "react";
+"use client";
+
+import React from 'react';
+import {useState} from "react"
+
+
+interface Pokemon {
+    id: number;
+    name: string;
+    height: number;
+    weight: number;}
+
 
 async function getPokemon({name}:{name:string}){
-    const res = await fetch("https://pokeapi.co/api/v2/berry/${name}", {
-        next ; {revalidate: 60}, 
+    const res = await fetch('https://pokeapi.co/api/v2/${name}', {
+        next : {revalidate: 60}, 
     });
     if (!res.ok) {
-        throw new Error("Failed to fetch");
+        throw new Error('Failed to fetch');
     }
     return res.json();
 }
 
-
 export default function Home() {
-    return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+            <button className="p-4 bg-blue-500 text-white rounded">Summit</button>
+         </div>
+    );
 }
+
