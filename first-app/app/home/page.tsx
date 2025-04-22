aae22266072c8f83a3b03b4293e99a9651fb9757
Sebastianpc2001 <73415184+Sebastianpc2001@users.noreply.github.com>
2025-04-22 15:32:22 +0200

    Update user input and console print

--- first-app/app/home/page.tsx
+++ first-app/app/home/page.tsx
@@ -22,11 +22,34 @@ async function getPokemon({name}:{name:string}){
 }
 
 export default function Home() {
+    const [pokemonName1, setPokemonName1] = useState("");
+    const [pokemon, setPokemon] = useState<Pokemon | null>(null);
+    const [pokemonName2, setPokemonName2] = useState("");
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        console.log("Summited the pokemon",pokemonName1);
+        console.log(pokemon)
+    const response =  await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${pokemonName1.toLowerCase()}`
+    );
+  
+    if (!response.ok) {
+        throw new Error("Pokémon not found");
+    }
+    const data = await response.json();
+    setPokemon(data);
+    }
     return (
         <div>
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>
-            <button className="p-4 bg-blue-500 text-white rounded">Summit</button>
+            <h1 className="text-3xl font-bold underline">Hello Pokemon Fan!</h1>
+            <form onSubmit={handleSubmit}>
+                <input value={pokemonName1} type="text" placeholder="Enter your pokemon" id="Pokemon" onChange={e=>setPokemonName1(e.target.value)}/>
+                <button type="submit" className="p-4 bg-blue-500 text-white rounded">Submit</button>
+            </form>
          </div>
-    );
+        );
 }
+
+
+
 
